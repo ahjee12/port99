c32f86d9990808f59b028888ac576937f9c5c814
안현지 <65323290+ahjee12@users.noreply.github.com>
2020-05-20 09:03:18 +0900

    2020.5.20

--- nongsim/assets/js/custom.js
+++ nongsim/assets/js/custom.js
@@ -1,5 +1,3 @@
-
-
 var mySwiper = new Swiper('#banner .swiper-container', {
   // Optional parameters
   loop: true,
@@ -18,12 +16,12 @@ var mySwiper = new Swiper('#banner .swiper-container', {
 
   autoplay: {
     delay: 5000,
-  } 
-}) 
- 
-var mySwiper2 = new Swiper('.notice .swiper-container', { 
+  }
+})
+
+var mySwiper2 = new Swiper('.notice .swiper-container', {
   // Optional parameters 
-  loop: true, 
+  loop: true,
   slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
@@ -35,7 +33,7 @@ var mySwiper2 = new Swiper('.notice .swiper-container', {
 var mySwiper3 = new Swiper('.story .swiper-container', {
   // Optional parameters
   loop: true,
-  slidesPerView:'auto',
+  slidesPerView: 'auto',
   // slidesPerView: 5,
   // spaceBetween: 30,
   centeredSlides: true,
@@ -44,3 +42,49 @@ var mySwiper3 = new Swiper('.story .swiper-container', {
   //   clickable: true
   // }
 })
+
+
+
+var mySwiper2 = new Swiper('.footerNav .swiper-container', {
+  // Optional parameters 
+  loop: false,
+  slidesPerView: 3,
+  spaceBetween: 0,
+  // pagination: {
+  //   el: '.footerNav .swiper-pagination',
+  //   clickable: true
+  // }
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+})
+
+var dragSize = 160;
+var swiper = new Swiper('.swiper-container', {
+  initialSlide: 0,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  scrollbar: {
+    el: '#footerNav .swiper-scrollbar',
+    dragSize: dragSize,
+    draggable: true
+  }
+});
+
+var mySwiper2 = new Swiper('.farmer .swiper-container', {
+  // Optional parameters 
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  // pagination: {
+  //   el: '.footerNav .swiper-pagination',
+  //   clickable: true
+  // }
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: 'true',
+  },
+})
+
