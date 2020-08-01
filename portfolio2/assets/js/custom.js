aef3dd1e12b199c03d5011e425e086db9638330b
안현지 <65323290+ahjee12@users.noreply.github.com>
2020-08-02 03:13:34 +0900

    2020.08.02

--- portfolio2/assets/js/custom.js
+++ portfolio2/assets/js/custom.js
@@ -4,3 +4,30 @@
 // headerMenu.hover(function(){
 //     $(this).css({'display':'block'},{'left':0});
 // })
+
+window.onload = function(){
+    var tl = new TimelineMax();
+    var home = $('.home');
+    var table = $('.table');
+    var hut = $('.hut');
+    var road = $('.road');
+    var greenhouse = $('.greenhouse');
+
+    // tl.set(home, {opacity: '0', left: '62.35%', top: '-100%'})
+    //   .to(home, {opacity: '1',left: '62.35%', top: '8.75%', duration: '1', ease:Bounce.easeOut})
+
+    tl.to(home, {opacity: '1',left: '62.35%', top: '8.75%', duration: '1', ease:Bounce.easeOut});
+
+
+}
+// var tl = new TimelineMax();
+//     var home = $('.home');
+//     var table = $('.table');
+//     var hut = $('.hut');
+//     var road = $('.road');
+//     var greenhouse = $('.greenhouse');
+
+//     // tl.set(home, {opacity: '0', left: '62.35%', top: '-100%'})
+//     //   .to(home, {opacity: '1',left: '62.35%', top: '8.75%', duration: '1', ease:Bounce.easeOut})
+
+//     tl.to(home, {opacity: '1',left: '62.35%', top: '8.75%', duration: '1', ease:Bounce.easeOut});
