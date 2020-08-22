2d2a9f3394e9e1869fa7f0f6c6b46f23f4a2e7bb
안현지 <65323290+ahjee12@users.noreply.github.com>
2020-08-22 19:06:21 +0900

    2020.08.22

--- portfolio2/assets/js/custom.js
+++ portfolio2/assets/js/custom.js
@@ -135,3 +135,19 @@ function ResetAllBoxs(){
       }
   })
 }
+
+/* --------패럴럭스------------ */
+var swiper = new Swiper('.item-list-parallax .swiper-container', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 50,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
